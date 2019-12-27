6f455cc91bd6bdf7a43a827ba1ca0662b274f236
mizo-0363 <gugu.net1977@gmail.com>
2019-12-28 05:05:56 +0900

    new-message

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,7 +20,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
