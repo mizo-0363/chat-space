7430b571240f6fb9e36aba66ece77414531efcd4
mizo-0363 <gugu.net1977@gmail.com>
2020-01-04 17:25:02 +0900

    インクルメンタルサーチ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_fit: [300, 300]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -28,9 +29,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-     process resize_to_fit: [800, 800]
-   end
+  # version :thumb do
+  #    process resize_to_fit: [800, 800]
+  #  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
