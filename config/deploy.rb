08147be9940063ee1702f1a08ea285c8df26800d
mizo-0363 <gugu.net1977@gmail.com>
2020-01-06 19:40:29 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:mizo_0363/chat-space.git'
+set :repo_url,  'git@github.com:mizo-0363/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,10 @@
+server '18.178.21.156', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "18.178.21.156", user: "ec2-user", roles: %w{app db web}, my_property: :my_value
+# server "18.178.21.156", user: "ec2-user", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
