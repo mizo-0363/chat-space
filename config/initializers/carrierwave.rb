860ac32a01f81d3407b2c16a54ad9ecbbfd0eed3
mizo-0363 <gugu.net1977@gmail.com>
2020-01-08 15:12:04 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'mizo-chatspaceimage'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mizo-chatspaceimage'
+  config.asset_host = 'https://mizo-chatspaceimage.s3.amazonaws.com'
 end
